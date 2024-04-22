b490a846b4af185b2ffcffa49633cba795a68456
daniellizarazoo <daniellizarazo015@gmail.com>
2024-04-22 14:53:25 -0500

    1.12 random anecdotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,149 +1,42 @@
 import { useState } from "react";
 
-const Display = ({counter}) => <div>Valor total {counter}</div>
-
-const Button = ({onClick,text}) => <button onClick={onClick}>{text}</button>
-
-const Titles = ({title}) => <h1>{title}</h1>
-
-const Statistics = ({text,counter}) => <p>{text} {counter}</p>
-
-const HtmlTable = (props) => {
-  return (
-    <table>
-    <thead>
-      <tr>
-        <th>{props.name}</th>
-        <th>{props.value}</th>
-      </tr>
-    </thead>
-  </table>
+const Titles = ({text}) => <h1>{text}</h1>
+const Buttons = ({text,onClick}) => {
+  console.log("BUTTON CLLICKED")
+  return(
+    <button onClick={onClick}>{text}</button>
   )
 }
+const Texts = (props) => <p>{props.text}</p>
 
 const App = () =>{
-  
-  const [left,setLeft] = useState(0)
-  const [right,setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-  const [value, setValue] = useState(10)
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [sumatoria, setSumatoria] = useState(0)
-  const [positiveAverage,setPositiveAverage]= useState(0)
+  const [selected, setSelected] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1 
-    setLeft(updatedLeft)
-    setTotal(updatedLeft+right)
-  }
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'Esto es aleatorio',
+    'Gracias Dios por encargarte de todas mis necesidades',
+    'Hay caminos que parecen derechos al hombre, más su fin es camino de muerde. Proverbios 16:25',
+    'Santificalos en tu verdad, tu palabra es verdad. Juan 17:17',
+    'La luz resplandeció en las tinieblas y las tinieblas no prevalecieron. Juan 1:5',
+    'Así como Moisés levantó la serpiente en el desierto, así también es necesario que el hijo del hombre sea levanta. Juan 3:14',
+    'Estád siempre gozosos 1 Tesalonicenses 5:16'
+  ]
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setTotal(left+right)
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left+updatedRight)
-  }
-
-  const handleClick = (name) => () => console.log('Hello',name)
-  
-  const handleSetValue = (newValue) => () => {
-    const oldValue = value
-    console.log("Valor a cambiar es " , oldValue, "valor a setear es ", newValue)
-    setValue(newValue)
-  }
-
-  const setToValue = (newValue) => ()=> {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
-
-  const handleGood = () => {
-    const newValue = good+1
-    setGood(newValue)
-    const lastTotal = total
-    setTotal(lastTotal+1)
-    funAverage(1,lastTotal+1)
-    promediadorPositivo(newValue,lastTotal+1)
-  }
-
-  const handleNeutral = () => {
-    const newValue = neutral+1
-    setNeutral(newValue)
-    const lastTotal = total
-    setTotal(lastTotal+1)
-    funAverage(0,lastTotal+1)
-    promediadorPositivo(good,lastTotal+1)
-  }
-
-  const handleBad = () => {
-    const newValue = bad+1
-    setBad(newValue)
-    const lastTotal = total
-    setTotal(lastTotal+1)
-    funAverage(-1,lastTotal+1)
-    promediadorPositivo(good,lastTotal+1)
-  }
-
-  const funAverage = (num,total) => {
-    const lastSum = sumatoria
-    const newValue = num + lastSum
-    setSumatoria(newValue)
-    const newAverage = total > 0 ? newValue / total : 0;
-    setAverage(newAverage)
-  }
-
-  const promediadorPositivo = (num, total) =>{
-    console.log("NUMERO DE POSITIVOS", num)
-    console.log("total ", total);
-    const newAverage = total > 0 ? num / total : 0;
-    setPositiveAverage(newAverage)
+  const handleNextAnecdote = () =>{
+    setSelected(Math.floor(Math.random() * 9))
   }
 
   return(
-    <div>
-      <Titles title="give feedback"/>
-      <Button text="good" onClick={handleGood}/>
-      <Button text="neutral" onClick={handleNeutral}/>
-      <Button text="bad" onClick={handleBad}/>
-      <Titles title="statistics"/>
-      
-      <HtmlTable name="Good" value={good}/>
-      <HtmlTable name="Neutral" value={neutral}/>
-      <HtmlTable name="Bad" value={bad}/>
-      <HtmlTable name="all" value={total}/>
-      <HtmlTable name="Average" value={average}/>
-      <HtmlTable name="Positive" value={positiveAverage}/>
-    </div>
+  <div>
+    <Titles text="Anécdotas:"/>
+    <Buttons text="Siguiente anecdota" onClick={handleNextAnecdote}/>
+    <Texts text ={anecdotes[selected]}/>
+  </div>
+  
+
   )
 }
-
-const Estadisticas = (props)=>{
-  if (props.total==0){
-    return(
-      <p>No feedback given</p>
-    )
-  }
-  else{
-    return(
-      <div>
-      <Statistics text="good  " counter={props.good}/>
-      <Statistics text="neutral  " counter={props.neutral}/>
-      <Statistics text="bad  " counter={props.bad}/>
-      <Statistics text="Total :" counter={props.total}/>
-      <Statistics text="Average :" counter={props.average}/>
-      <Statistics text="Positive average :" counter={props.positiveAverage}/>
-      </div>
-    )
-  }
-}
-
-
-
 
 export default App
