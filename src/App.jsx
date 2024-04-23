d4b4a5a8c924b7a982e42e3e87af863611fffd60
daniellizarazoo <daniellizarazo015@gmail.com>
2024-04-23 12:27:49 -0500

    1.14 Mayor cantidad de votos, conteo de votos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import { useState } from "react";
 
+// COMPONENTES
 const Titles = ({text}) => <h1>{text}</h1>
+
 const Buttons = ({text,onClick}) => {
-  console.log("BUTTON CLLICKED")
   return(
     <button onClick={onClick}>{text}</button>
   )
 }
 const Texts = (props) => <p>{props.text}</p>
 
+const MostVotes = ({votes,anecdotes}) => {
+  const maximo = Math.max(...votes)
+  const indiceMaximo = votes.indexOf(maximo)
+  if (maximo<1){
+    return(
+      <p> Aun no se registran votos</p>
+    )
+  }
+  else{
+    return(
+      <Texts text ={`${anecdotes[indiceMaximo]} has ${maximo}`}/>
+    )
+  }
+}
+
 const App = () =>{
   const [selected, setSelected] = useState(0)
-
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -23,16 +38,27 @@ const App = () =>{
     'Así como Moisés levantó la serpiente en el desierto, así también es necesario que el hijo del hombre sea levanta. Juan 3:14',
     'Estád siempre gozosos 1 Tesalonicenses 5:16'
   ]
+  const [votes, setVotes] = useState([...Array(anecdotes.length)].fill(0))
 
   const handleNextAnecdote = () =>{
     setSelected(Math.floor(Math.random() * 9))
   }
 
+  const handleVotes = () => {
+    const actualVotes= [...votes]
+    actualVotes[selected]+=1
+    setVotes(actualVotes)
+  }
+
   return(
   <div>
-    <Titles text="Anécdotas:"/>
-    <Buttons text="Siguiente anecdota" onClick={handleNextAnecdote}/>
+    <Titles text="Anecdote of the day:"/>
     <Texts text ={anecdotes[selected]}/>
+    <Buttons text="Siguiente anecdota" onClick={handleNextAnecdote}/>
+    <Buttons text="Votar" onClick={handleVotes}/>
+    <Texts text ={`Votos: ${votes[selected]}`}/>
+    <Titles text="Anecdote wiht most votes:"/>
+    <MostVotes votes={votes} anecdotes={anecdotes}/>
   </div>
   
 
