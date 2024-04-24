97033fe122ea09d2c2842726d71b0da8b9d58276
daniellizarazoo <daniellizarazo015@gmail.com>
2024-04-24 17:51:06 -0500

    practicando con forms

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const notes = [
+    {
+        content: 'HTML is easy', id: 1, important : true
+    },
+    {
+        content: 'Browser can execute only javascript', id: 2, important : false
+    },
+    {
+        content: 'GET and POST are the most important HTTP methods', id: 3, important : true
+    },
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
+    <App notes={notes}/>
 )
 
 
